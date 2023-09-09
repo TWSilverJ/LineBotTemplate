8986e6b9839853b6a355837c0f8528a949afef37
SilverJ <jack61606@hotmail.com>
2023-09-09 19:16:27 +0800

    安裝 dotenv 套件，處理應用程式參數

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,6 +1,17 @@
 import createError from 'http-errors'
+import dotenv from 'dotenv'
 import express from 'express'
 import logger from 'morgan'
+
+// 從環境變數導入應用程式設定
+dotenv.config()
+const { env } = process
+const config = {
+    env: env.NODE_ENV,
+    debug: env.DEBUG?.toLowerCase() === 'true' || false,
+    port: Number(env.PORT),
+    url: env.APP_URL ? env.APP_URL : `http://127.0.0.1:${Number(env.PORT)}`
+}
 
 // 實作 express
 const app = express()
@@ -21,14 +32,14 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+    res.locals.error = config.debug ? err : {}
 
     // render the error page
     res.status(err.status || 500)
-    res.json({ message: 'error' })
+    res.send('error')
 })
 
 // 監聽電腦指定 port
-app.listen(3000, async () => {
-    console.log(new Date().toISOString(), '伺服器啟動')
+app.listen(config.port, async () => {
+    console.log(new Date().toISOString(), `伺服器啟動 ${config.url}`)
 })
